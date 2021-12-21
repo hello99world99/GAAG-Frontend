fc6282f3368dc6be172e9f5460ef5db004cb1eb8
Ibrahim Kelly <57602469+hello99world99@users.noreply.github.com>
2021-12-21 15:06:51 +0000

    Selection de la promotion effectuée...

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions } from '@ionic/core';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public content = 'Promotion';
+  constructor(private pickerCtrl: PickerController) {}
 
-  constructor() {}
+  public async showContent() {
+    const opts: PickerOptions = {
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'Cancel',
+        },
+        {
+          text: 'Done',
+        }
+      ],
+      columns: [
+        {
+          name: 'promotion',
+          options: [
+            {text: 'Python', value: 'Python language'},
+            {text: 'Java', value: 'Java language'},
+            {text: 'JavaScript', value: 'JavaScript language'},
+            {text: 'TypeScript', value: 'TypeScript language'}
+          ]
+        }
+      ],
+    };
+    const picker = await this.pickerCtrl.create(opts);
+    picker.present();
+    picker.onDidDismiss().then( async data => {
+      const col = await picker.getColumn('promotion');
+      this.content = col.options[col.selectedIndex].text;
+    });
+  }
 
 }
